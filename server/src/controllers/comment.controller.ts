35793204d08bc946e0c877aab216b4e32b40706e
makkenzo <nekgo2009@gmail.com>
2023-09-27 15:41:51 +0600

    updated comment controller + routes

--- server/src/controllers/comment.controller.ts
+++ server/src/controllers/comment.controller.ts
@@ -22,3 +22,64 @@ export const createComment = async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 };
+
+export const getAllComments = async (req: Request, res: Response) => {
+    try {
+        const comments = await CommentModel.find();
+        res.status(200).json(comments);
+    } catch (error) {
+        console.error('Error fetching comments:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+export const getCommentById = async (req: Request, res: Response) => {
+    try {
+        const commentId = req.params.id;
+        const comment = await CommentModel.findById(commentId);
+
+        if (!comment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+
+        res.status(200).json(comment);
+    } catch (error) {
+        console.error('Error fetching comment by ID:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+export const updateComment = async (req: Request, res: Response) => {
+    try {
+        const commentId = req.params.id;
+        const updateData = req.body;
+
+        const updatedComment = await CommentModel.findByIdAndUpdate(commentId, updateData, { new: true });
+
+        if (!updatedComment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+
+        res.status(200).json(updatedComment);
+    } catch (error) {
+        console.error('Error updating comment:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+export const deleteComment = async (req: Request, res: Response) => {
+    try {
+        const commentId = req.params.id;
+
+        const deletedComment = await CommentModel.findByIdAndRemove(commentId);
+
+        if (!deletedComment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+
+        res.status(204).send(); // 204 No Content
+    } catch (error) {
+        console.error('Error deleting comment:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
---
+++ server/src/routes/comment.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import * as CommentController from '../controllers/comment.controller';
+
+const commentRouter = Router();
+
+commentRouter.post('/create', CommentController.createComment);
+commentRouter.get('/', CommentController.getAllComments);
+commentRouter.get('/:id', CommentController.getCommentById);
+commentRouter.put('/:id', CommentController.updateComment);
+commentRouter.delete('/:id', CommentController.deleteComment);
+
+export default commentRouter;
