5f5f54868169036556e5cadbb48ce90b05234397
makkenzo <nekgo2009@gmail.com>
2023-09-27 15:36:48 +0600

    user routes

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import UserModel from '../models/user.model';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = async (req: Request, res: Response) => {
     try {
@@ -14,9 +16,11 @@ export const registerUser = async (req: Request, res: Response) => {
             return res.status(409).json({ message: 'User with this username already exists' });
         }
 
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         const user = new UserModel({
             username,
-            password,
+            hashedPassword,
             role: role || 'user',
         });
 
@@ -25,6 +29,41 @@ export const registerUser = async (req: Request, res: Response) => {
         res.status(201).json({ message: 'User registered successfully' });
     } catch (err) {
         console.error('Error registering user:', err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body;
+
+        // Проверка наличия обязательных полей
+        if (!username || !password) {
+            return res.status(400).json({ message: 'Username and password are required' });
+        }
+
+        // Поиск пользователя в базе данных
+        const user = await UserModel.findOne({ username });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        // Сравнение пароля пользователя с хэшированным паролем в базе данных
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        // Генерация JWT-токена для аутентифицированного пользователя
+        const token = jwt.sign({ userId: user._id, username: user.username }, process.env.JWT_SECRET, {
+            expiresIn: '1h', // Продолжительность жизни токена
+        });
+
+        res.status(200).json({ token, userId: user._id, username: user.username });
+    } catch (error) {
+        console.error('Error logging in user:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
 };
